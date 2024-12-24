012146a596d8458044353a8eb6be7b25959111c7
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-24 13:19:32 +0530

    Refactor Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,17 @@
-import Image from "next/image";
+import HeroSection from './components/HeroSection'
+import VideoSection from './components/VideoSection'
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen bg-khimji-red overflow-hidden flex flex-col items-center justify-center">
-      {/* Logo - Top Left */}
-      <div className="absolute top-8 left-8 z-10">
-        <Image
-          src="/images/khimji-logo.png"
-          alt="Khimji Jewellers"
-          width={150}
-          height={60}
-          priority
-        />
-      </div>
-
-      {/* Pattern - Top Right */}
-      <div className="absolute top-0 right-0 opacity-20">
-        <Image
-          src="/images/pattern.png"
-          alt="Decorative Pattern"
-          width={400}
-          height={400}
-          className="object-contain"
-        />
-      </div>
-
-      {/* Main Content */}
-      <div className="flex flex-col items-center justify-center">
-        {/* Main Illustration */}
-        <div className="relative w-full max-w-2xl mx-auto px-4">
-          <Image
-            src="/images/illustration.png"
-            alt="Khimji Illustration"
-            width={500}
-            height={500}
-            className="object-contain"
-            priority
-          />
+    <main className="min-h-screen min-w-screen">
+      <div className="flex flex-col lg:flex-row">
+        <div className="w-full lg:w-[60%]">
+          <HeroSection />
         </div>
-
-        {/* Coming Soon Text */}
-        <div className="mt-10">
-            <span className="text-white text-2xl font-normal tracking-[.90em]">
-              COMING SOON
-            </span>
+        <div className="w-full lg:w-[40%] relative">
+          <VideoSection />
         </div>
       </div>
-    </div>
-  );
+    </main>
+  )
 }
