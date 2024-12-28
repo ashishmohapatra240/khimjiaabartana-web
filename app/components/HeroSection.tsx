ceac8e35cc98116b2a34ca95a0a54cecf6632043
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-12-28 14:27:18 +0530

    Update HeroSection and VideoSection

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -3,7 +3,7 @@ import { archivo, notoSansOriya } from "../fonts";
 
 export default function HeroSection() {
   return (
-    <section className="bg-[#bd1621] h-screen px-6 sm:px-8 md:px-12 lg:px-16 py-8 flex flex-col items-start justify-start">
+    <section className="bg-[#b81d18] h-screen px-6 sm:px-8 md:px-12 lg:px-16 py-8 flex flex-col items-start justify-start">
       {/* Logos Section */}
       <div className="flex items-center justify-start gap-6 sm:gap-8 mb-12 sm:mb-16">
         <div className="relative w-32 sm:w-40 h-20 sm:h-24">
@@ -46,7 +46,7 @@ export default function HeroSection() {
 
       {/* Button Section */}
       <button
-        className={`${archivo.className} mt-12 sm:mt-16 px-8 sm:px-10 md:px-12 py-3 sm:py-4 bg-khimji-gold text-khimji-red font-medium text-sm sm:text-base md:text-lg uppercase hover:bg-opacity-90 transition-all duration-300 whitespace-nowrap`}
+        className={`${archivo.className} mt-12 sm:mt-16 px-8 sm:px-10 md:px-12 py-3 sm:py-4 bg-[#CBA20E] text-khimji-red font-medium text-sm sm:text-base md:text-lg uppercase hover:bg-opacity-90 transition-all duration-300 whitespace-nowrap`}
       >
         Explore Khimji Aabartana 2025
       </button>
--- app/components/VideoSection.tsx
+++ app/components/VideoSection.tsx
@@ -6,7 +6,7 @@ export default function VideoSection() {
         loop
         muted
         playsInline
-        className="w-full h-full absolute scale-[2] duration-500 overflow-hidden md:ml-60 md:mt-0 mt-80"
+        className="w-full h-full absolute scale-[1.9] hover:scale-[2] duration-500 overflow-hidden md:ml-80 md:mt-0 mt-96 md:py-0 py-16"
       >
         {/* <source src="/videos/Hero.mp4" type="video/quicktime" /> */}
         <source src="/videos/Hero.mp4" />
