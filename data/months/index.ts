a63be94974ec604349215badfadbcedba9bc2558
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-01-03 21:51:21 +0530

    Add months data aggregation in index.ts

---
+++ data/months/index.ts
@@ -0,0 +1,29 @@
+import { januaryData } from './january';
+import { februaryData } from './february';
+import { marchData } from './march';
+import { aprilData } from './april';
+import { mayData } from './may';
+import { juneData } from './june';
+import { julyData } from './july';
+import { augustData } from './august';
+import { septemberData } from './september';
+import { octoberData } from './october';
+import { novemberData } from './november';
+import { decemberData } from './december';
+
+export type MonthData = typeof januaryData;
+
+export const monthsData: Record<number, MonthData> = {
+    1: januaryData,
+    2: februaryData,
+    3: marchData,
+    4: aprilData,
+    5: mayData,
+    6: juneData,
+    7: julyData,
+    8: augustData,
+    9: septemberData,
+    10: octoberData,
+    11: novemberData,
+    12: decemberData,
+};
