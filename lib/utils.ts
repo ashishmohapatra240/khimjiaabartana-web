a99ba59bd2123756979668277f34d2081bda18c9
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-01-03 13:14:26 +0530

    Format Date with LeadingZero

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatDateWithLeadingZero = (date: number): string => {
+  return date < 10 ? `0${date}` : `${date}`;
+};
