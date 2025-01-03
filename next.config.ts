c7bab93d30f4e164408c2ed1e0467104790d9437
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-01-03 21:54:01 +0530

    Refactor Next.js image configuration to use remotePatterns for raw.githubusercontent.com

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   domains: ["raw.githubusercontent.com"],
+  // },
   images: {
-    domains: ["raw.githubusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
   },
 };
 
