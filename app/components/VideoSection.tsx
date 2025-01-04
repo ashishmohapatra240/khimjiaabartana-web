38b272385021fd41c393c6749c0834ca07e82a02
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2025-01-04 23:02:34 +0530

    Update VideoSection component styles and add responsive video sources

--- app/components/VideoSection.tsx
+++ app/components/VideoSection.tsx
@@ -1,15 +1,16 @@
 export default function VideoSection() {
   return (
-    <section className="h-screen overflow-hidden relative">
+    <section className="h-[50vh] md:h-[100vh] overflow-hidden relative md:bg-[#aa030e] bg-[#b81d18]">
       <video
         autoPlay
         loop
         muted
         playsInline
-        className="w-full h-full absolute scale-[1.9] hover:scale-[2] duration-500 overflow-hidden md:ml-80 md:mt-0 mt-20 md:py-0 py-16"
+        className="w-full absolute top-0 md:scale-[1.9] md:hover:scale-[2] duration-500
+          object-cover object-top md:ml-80 md:mt-0 mt-36 md:py-0 scale-[1.4] "
       >
-        {/* <source src="/videos/Hero.mp4" type="video/quicktime" /> */}
-        <source src="/media/Desktop.mp4" />
+        <source src="/media/Phone.mp4" media="(max-width: 768px)" />
+        <source src="/media/Desktop.mp4" media="(min-width: 769px)" />
       </video>
     </section>
   );
